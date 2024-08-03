f30f62a22311b5e1ccef88e0a25003518c42d78d
Awhux <brudoismilequatro@gmail.com>
2024-08-03 12:30:11 -0300

    test: add initial scaffold for file validator unit tests

---
+++ tests/file_validator_test.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "../src/file_validator.hpp"
+
+// TEST(FileValidatorTest, ValidateValidFile)
+// {
+//     EXPECT_TRUE(FileValidator::validate("test_files/valid.xlsx"));
+// }
+
+// TEST(FileValidatorTest, ValidateInvalidFile)
+// {
+//     EXPECT_FALSE(FileValidator::validate("test_files/invalid.txt"));
+// }
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
